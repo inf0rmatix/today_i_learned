6c9928da3302f247f2ba135fa803dfc6f418667c
Paul Köhler <kontakt@paul-koehler.dev>
2022-03-27 15:11:35 +0200

    created app spacing class

---
+++ lib/src/config/app_spacing.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: constant_identifier_names
+
+class AppSpacing {
+  static const double XL = 2 * L;
+  static const double L = 1.5 * M;
+  static const double M = 8;
+  static const double S = 0.75 * M;
+  static const double XS = 0.5 * S;
+}
