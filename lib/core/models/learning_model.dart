e0f3aa42405c52d3333e26e5d0ca0daf9389e012
1nf0rmatix <102466036+1nf0rmatix@users.noreply.github.com>
2022-04-06 19:37:35 +0200

    freezing branch with very good cli being not so very good but rather restrictive

---
+++ lib/core/models/learning_model.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'learning_model.freezed.dart';
+part 'learning_model.g.dart';
+
+@freezed
+class LearningModel with _$LearningModel {
+  factory LearningModel({
+    required String uid,
+    required String title,
+    required String description,
+    required String difficulty,
+    required String category,
+  }) = _LearningModel;
+
+  factory LearningModel.fromJson(Map<String, dynamic> json) =>
+      _$LearningModelFromJson(json);
+}
