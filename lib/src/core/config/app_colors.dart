1571ed1e380d23b5ccf77805ea755b1ce897afe5
1nf0rmatix <102466036+1nf0rmatix@users.noreply.github.com>
2022-04-10 14:14:28 +0200

    updating app colors

--- lib/src/core/config/app_colors.dart
+++ lib/src/core/config/app_colors.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
+  static const olivine = Color(0xFF8CB369);
+  static const flax = Color(0xFFF4E285);
+  static const sandyBrown = Color(0xFFF4A259);
+  static const wintergreenDream = Color(0xFF5B8E7D);
+  static const bittersweetShimmer = Color(0xFFBC4B51); // error
+
   static const emerald = Color(0xFF439A86);
   static const lavender = Color(0xFFCABAC8);
   static const blue = Color(0xFF197BBD);
--- lib/src/core/config/app_theme.dart
+++ lib/src/core/config/app_theme.dart
@@ -6,24 +6,11 @@ class AppTheme {
 
   static final light = ThemeData.from(
     colorScheme: const ColorScheme.light(
-      primary: AppColors.emerald,
-      secondary: AppColors.lavender,
-      tertiary: AppColors.yellow,
+      primary: AppColors.wintergreenDream,
+      secondary: AppColors.sandyBrown,
+      tertiary: AppColors.flax,
+      error: AppColors.bittersweetShimmer,
+      background: AppColors.olivine,
     ),
-    // colorScheme: const ColorScheme(
-    //   brightness: Brightness.dark,
-    //   primary: AppColors.blue,
-    //   onPrimary: Colors.white,
-    //   secondary: AppColors.emerald,
-    //   onSecondary: Colors.white,
-    //   tertiary: AppColors.darkGreen,
-    //   onTertiary: Colors.white,
-    //   background: AppColors.darkGreen,
-    //   onBackground: Colors.white,
-    //   error: Colors.red,
-    //   onError: Colors.white,
-    //   surface: AppColors.lavender,
-    //   onSurface: Colors.white,
-    // ),
   );
 }
